5d32a4ea7e7e8c44be173eafca39310d4ea6c0cf
Ashir Ahmed <ashirahmed810@gmail.com>
2025-07-23 22:09:03 +0500

    ad

--- login/lib/login.dart
+++ login/lib/login.dart
@@ -6,39 +6,58 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        // color: Colors.amber,
-        // margin: EdgeInsets.only(top: 150),
-        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 150),
-        child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text("Log in", style: TextStyle(fontSize: 33)),
-            SizedBox(height: 45),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(6),
+      body: SingleChildScrollView(
+        child: Container(
+          // color: Colors.amber,
+          // margin: EdgeInsets.only(top: 150),
+          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 150),
+          child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Log in", style: TextStyle(fontSize: 33)),
+              SizedBox(height: 45),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  hintText: "Email",
+                  hintStyle: TextStyle(fontSize: 20),
                 ),
-                hintText: "Email",
-                hintStyle: TextStyle(fontSize: 20),
               ),
-            ),
-            SizedBox(height: 30),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(6),
+              SizedBox(height: 30),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  hintText: "Password",
+                  hintStyle: TextStyle(fontSize: 20),
                 ),
-                hintText: "Password",
-                hintStyle: TextStyle(fontSize: 20),
               ),
-            ),
-            SizedBox(height: 30),
+              SizedBox(height: 30),
 
-            ElevatedButton(onPressed: () {}, child: Text("Login")),
-          ],
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  padding: EdgeInsets.symmetric(horizontal: 120),
+                ),
+                child: Text(
+                  "Log in",
+                  style: TextStyle(
+                    color: Colors.white, // ✅ Now this works!
+                    fontSize: 17,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+
+              Row(),
+            ],
+          ),
         ),
       ),
     );
