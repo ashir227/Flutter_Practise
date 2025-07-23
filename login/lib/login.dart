c2a2f7c142c7c78c15aac1ac1098aeed4116fe4c
Ashir Ahmed <ashirahmed810@gmail.com>
2025-07-23 20:52:40 +0500

    New

--- login/lib/login.dart
+++ login/lib/login.dart
@@ -7,11 +7,35 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.only(left: 30),
-        padding: EdgeInsets.symmetric(vertical: 200),
+        // color: Colors.amber,
+        // margin: EdgeInsets.only(top: 150),
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 150),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Text("Log in", style: TextStyle(fontSize: 33))],
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Log in", style: TextStyle(fontSize: 33)),
+            SizedBox(height: 45),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                hintText: "Email",
+                hintStyle: TextStyle(fontSize: 20),
+              ),
+            ),
+            SizedBox(height: 30),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                hintText: "Password",
+                hintStyle: TextStyle(fontSize: 20),
+              ),
+            ),
+          ],
         ),
       ),
     );
