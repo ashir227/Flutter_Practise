f7b52ff7e39014ea87b499ae73d0728c45867b65
Ashir Ahmed <ashirahmed810@gmail.com>
2025-07-25 19:24:11 +0500

    Add Set

--- login/lib/counter.dart
+++ login/lib/counter.dart
@@ -8,23 +8,39 @@ class counterview extends StatefulWidget {
 }
 
 class _CounterState extends State<counterview> {
-  int counter = 0;
+  bool isfav = false;
+  // int counter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.grey, title: Text("Counter App")),
-      body: Column(
-        children: [
-          Text("$counter"),
-          SizedBox(height: 13),
-          ElevatedButton(
-            onPressed: () {
-              counter++;
-              setState(() {});
-            },
-            child: Text("count"),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  isfav = !isfav;
+                });
+              },
+              icon: Icon(
+                Icons.favorite,
+                size: 60,
+                color: isfav ? Colors.red : Colors.purple,
+              ),
+            ),
+            // Text("$counter", style: TextStyle(fontSize: 40)),
+            // SizedBox(height: 13),
+            // ElevatedButton(
+            //   onPressed: () {
+            //     counter++;
+            // setState(() {});
+            //   },
+            // child: Text("count", style: TextStyle(fontSize: 30)),
+            // ),
+          ],
+        ),
       ),
     );
   }
