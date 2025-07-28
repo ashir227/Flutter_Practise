b0ea5f5dd98a1e41afc1032d20b0e2c619f7673a
Ashir Ahmed <ashirahmed810@gmail.com>
2025-07-28 21:55:39 +0500

    Add Widgets

--- login/lib/UI2.dart
+++ login/lib/UI2.dart
@@ -8,36 +8,54 @@ class Ui2 extends StatefulWidget {
 }
 
 class _Ui2State extends State<Ui2> {
+  bool isfav = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: 250,
-        // margin: EdgeInsets.only(bottom: 200),
-        padding: EdgeInsets.only(bottom: 130),
-        color: Colors.amber,
-        child: Row(
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.arrow_back_ios, size: 33),
+      body: SingleChildScrollView(
+        child: Container(
+          // decoration: BoxDecoration(borderRadius: BorderRadius.circular(33)),
+          height: 250,
+          // margin: EdgeInsets.only(bottom: 200),
+          padding: EdgeInsets.only(bottom: 130),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(215, 48, 7, 255),
+            borderRadius: BorderRadius.only(
+              // bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(120),
             ),
-            Spacer(),
-            Text(
-              "ENGLISH",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+          ),
+          child: Row(
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.arrow_back_ios, size: 33, color: Colors.white),
               ),
-            ),
-            Spacer(),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.ac_unit_sharp, size: 44),
-            ),
-          ],
+              Spacer(),
+              Text(
+                "ENGLISH",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Spacer(),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    isfav = !isfav;
+                  });
+                },
+                icon: Icon(
+                  Icons.ac_unit_sharp,
+                  size: 44,
+                  color: isfav ? Colors.white : Colors.greenAccent,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
