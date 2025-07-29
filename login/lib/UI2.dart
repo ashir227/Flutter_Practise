425b82d0c00c4c8cf7e32210096da768aa020ce2
Ashir Ahmed <ashirahmed810@gmail.com>
2025-07-29 19:17:56 +0500

    Text field

--- login/lib/UI2.dart
+++ login/lib/UI2.dart
@@ -12,104 +12,117 @@ class _Ui2State extends State<Ui2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: 250,
-        color: const Color.fromARGB(255, 54, 11, 228),
-
-        child: Column(
-          children: [
-            SizedBox(height: 35),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.arrow_back_ios,
-                    size: 25,
-                    color: Colors.white,
-                  ),
-                ),
-                Text(
-                  "ENGLISH",
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      isfav = !isfav;
-                    });
-                  },
-                  icon: Icon(
-                    Icons.account_circle_rounded,
-                    size: 40,
-                    color: isfav ? Colors.white : Colors.yellow,
-                  ),
-                ),
-              ],
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 54, 11, 228),
+              borderRadius: BorderRadius.only(bottomRight: Radius.circular(80)),
             ),
-            Container(
-              // height: 100,
-              color: Colors.amber,
-              margin: EdgeInsets.only(left: 32, top: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            width: double.infinity,
+            height: 250,
+
+            child: Container(
+              child: Column(
                 children: [
-                  Text.rich(
-                    TextSpan(
+                  SizedBox(height: 35),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.arrow_back_ios,
+                          size: 25,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "ENGLISH",
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isfav = !isfav;
+                          });
+                        },
+                        icon: Icon(
+                          Icons.account_circle_rounded,
+                          size: 40,
+                          color: isfav ? Colors.white : Colors.yellow,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    // height: 100,
+                    // color: Colors.amber,
+                    margin: EdgeInsets.only(left: 32, top: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        TextSpan(
-                          text: "Unit ",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 43,
-                            fontWeight: FontWeight.bold,
+                        Text.rich(
+                          TextSpan(
+                            children: [
+                              TextSpan(
+                                text: "Unit ",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 43,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              TextSpan(
+                                text: "2",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w800,
+                                  fontSize: 35,
+                                  color: Colors.yellowAccent,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        TextSpan(
-                          text: "2",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 35,
-                            color: Colors.yellowAccent,
+                        Container(
+                          margin: EdgeInsets.only(right: 20),
+                          child: Row(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.local_grocery_store_sharp,
+                                  size: 40,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ],
                     ),
                   ),
-                  Row(
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.account_circle_rounded,
-                          size: 40,
-                          color: Colors.cyan,
+                  Container(
+                    margin: EdgeInsets.only(left: 30),
+                    child: Row(
+                      children: [
+                        Text(
+                          "JOBS AND SCHOOLS",
+                          style: TextStyle(color: Colors.white, fontSize: 28),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(left: 30),
-              child: Row(
-                children: [
-                  Text(
-                    "JOBS AND SCHOOLS",
-                    style: TextStyle(color: Colors.white, fontSize: 28),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+          Container(child: Column(children: [TextField()])),
+        ],
       ),
     );
   }
